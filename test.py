d94d0c7fff23ee50b8ad258fe27f3310d7abc58d
Belattou sirine <sirinefzbelattou@gmailcom>
2026-06-11 16:37:07 +0100

    Add requirements.txt and clean up files

--- test.py
+++
@@ -1,4 +0,0 @@
-import requests
-r = requests.get("https://opensky-network.org/api/states/all", timeout=10)
-print(f"Status Code: {r.status_code}")
-print(f"Response: {r.text[:200]}") # Print first 200 chars
