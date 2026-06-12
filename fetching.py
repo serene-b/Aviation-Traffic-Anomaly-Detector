f3246c8e2af69c77f2a2cb71f0461260effd2b68
Belattou sirine <sirinefzbelattou@gmailcom>
2026-06-12 10:24:22 +0100

    added gitignore, use streamlit secrets for API auth

--- fetching.py
+++ fetching.py
@@ -1,14 +1,14 @@
 import requests
-
+import streamlit as st
 def fetch_fights():
     try:
         response = requests.post(
     "https://auth.opensky-network.org/auth/realms/opensky-network/protocol/openid-connect/token",
     data={
-        "grant_type": "client_credentials",
-        "client_id": "sirinefzbelattou-api-client",
-        "client_secret": "CfX6o15biTmRKEuXDDOng4heSBUqYcad"
-    }
+    "grant_type": "client_credentials",
+    "client_id": st.secrets["CLIENT_ID"],
+    "client_secret": st.secrets["CLIENT_SECRET"]
+}
 )
         token = response.json()["access_token"]
         r = requests.get(
