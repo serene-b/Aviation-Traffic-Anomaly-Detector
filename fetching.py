d0db96dc53493122dc710754ebe71546b6dff258
Belattou sirine <sirinefzbelattou@gmailcom>
2026-06-12 10:51:55 +0100

    restore fetching and app, clean working version

--- fetching.py
+++ fetching.py
@@ -2,23 +2,20 @@ import requests
 import streamlit as st
 
 def fetch_fights():
-    try:
-        response = requests.post(
-            "https://auth.opensky-network.org/auth/realms/opensky-network/protocol/openid-connect/token",
-            data={
-                "grant_type": "client_credentials",
-                "client_id": st.secrets["CLIENT_ID"],
-                "client_secret": st.secrets["CLIENT_SECRET"]
-            },
-            timeout=10
-        )
-        token = response.json()["access_token"]
-        r = requests.get(
-            "https://opensky-network.org/api/states/all",
-            headers={"Authorization": "Bearer " + token},
-            timeout=10
-        )
-        data = r.json()
-        return data
-    except Exception as e:
-        return None
+    response = requests.post(
+        "https://auth.opensky-network.org/auth/realms/opensky-network/protocol/openid-connect/token",
+        data={
+            "grant_type": "client_credentials",
+            "client_id": st.secrets["CLIENT_ID"],
+            "client_secret": st.secrets["CLIENT_SECRET"]
+        },
+        timeout=10
+    )
+    token = response.json()["access_token"]
+    r = requests.get(
+        "https://opensky-network.org/api/states/all",
+        headers={"Authorization": "Bearer " + token},
+        timeout=10
+    )
+    data = r.json()
+    return data
