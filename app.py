bc99c76bcfaa6ca0c321eb23ba5bfd8c4e93286a
Belattou sirine <sirinefzbelattou@gmailcom>
2026-06-12 10:38:25 +0100

    debug error

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from cleaning import cleaning_data
 from detecting import detecting_anomalies
 from mapping import mapping
 from streamlit_folium import st_folium
-st.write(st.secrets)
+
 st.set_page_config(page_title="Flight Anomaly Detector", layout="wide")
 st_autorefresh(interval=600000, key="refresh")
 st.title("✈️ Live Flight Anomaly Detector")
@@ -16,3 +16,8 @@ st.sidebar.metric("total number of clean flights: ", len(clean_flights))
 st.sidebar.metric("Number of Detected anomalies (rule based): ", len(anomalies_found))
 st.sidebar.metric("Number of Detected anomalies (Ml flags): ", len(detected_anomalies))
 st_folium(map, width=1400, height=600)
+try:
+    clean_flights, anomalies_found, total_flights = cleaning_data()
+except Exception as e:
+    st.error(f"Error: {e}")
+    st.stop()
