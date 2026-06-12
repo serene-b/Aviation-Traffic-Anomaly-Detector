4259a7db0648a77892834a038b6ea4e79e902b55
Belattou sirine <sirinefzbelattou@gmailcom>
2026-06-12 10:43:41 +0100

    add timeout to token request

--- fetching.py
+++ fetching.py
@@ -1,21 +1,23 @@
 import requests
 import streamlit as st
+
 def fetch_fights():
     try:
         response = requests.post(
-    "https://auth.opensky-network.org/auth/realms/opensky-network/protocol/openid-connect/token",
-    data={
-    "grant_type": "client_credentials",
-    "client_id": st.secrets["CLIENT_ID"],
-    "client_secret": st.secrets["CLIENT_SECRET"]
-}
-)
+            "https://auth.opensky-network.org/auth/realms/opensky-network/protocol/openid-connect/token",
+            data={
+                "grant_type": "client_credentials",
+                "client_id": st.secrets["CLIENT_ID"],
+                "client_secret": st.secrets["CLIENT_SECRET"]
+            },
+            timeout=10
+        )
         token = response.json()["access_token"]
         r = requests.get(
-    "https://opensky-network.org/api/states/all",
-    headers={"Authorization": "Bearer " + token},
-    timeout=10
-)
+            "https://opensky-network.org/api/states/all",
+            headers={"Authorization": "Bearer " + token},
+            timeout=10
+        )
         data = r.json()
         return data
     except Exception as e:
